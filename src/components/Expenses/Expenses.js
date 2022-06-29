45dde0fc03216c1792a53e05b5fa2c5f4b9f358b
dlrlseong <leekisung0212@gmail.com>
2022-06-29 17:16:36 +0900

    조건부 렌더링 ver.1
    &&

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,28 +8,30 @@ const Expenses = (props) => {
 
   const filterChangeHandler = (year) => {
     setSelectedYear(year);
-    console.log('Expenses.js!');
+    console.log("Expenses.js!");
     console.log(year);
   };
 
-  const filteredExpenses = props.items.filter(expenses => {
+  const filteredExpenses = props.items.filter((expenses) => {
     return expenses.date.getFullYear().toString() === selectedYear;
-  })
-  
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         onFilterChange={filterChangeHandler}
         selected={selectedYear}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-        key = {expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-      ))}
+      {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
     </Card>
   );
 };
